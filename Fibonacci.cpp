fa603bfe9d28e3f7984bd8578d12bcb476030a92
polcraz <polcrazpolcraz@gmail.com>
2018-05-19 11:07:22 +0300

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -7,16 +7,7 @@ using namespace std;
 
 int fib(int n)
 {
-	if (n == 0) { return 0; }
-	else
-	{
-		if ((n == -1) || (n == 1)) { return 1; }
-		else
-		{
-			if (n > 0) { return fib(n - 1) + fib(n - 2); }
-			else { return fib(n + 2) - fib(n + 1); }
-		}
-	}
+	return round((pow((1+sqrt(5))/2, n) / sqrt(5)));
 }
 int main()
 {
